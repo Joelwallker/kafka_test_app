33d5dd262d354b49f4ef1f6189c782fac8145235
Whitehercog <61663639+Joelwallker@users.noreply.github.com>
2023-12-01 13:26:43 +0300

    Delete view directory

--- view/kafka_view.py
+++
@@ -1,6 +0,0 @@
-# kafka_view.py
-
-class KafkaView:
-    @staticmethod
-    def display_stats(server, topic, rps, sent, consumed, lag_info):
-        print(f"Server: {server}, Topic: {topic}, RPS: {rps}, Sent: {sent}, Consumed: {consumed}, Lag: {lag_info}")
